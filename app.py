257b08c54de2c2f363c30f4b5910380d7a45a2df
riottecboi <tranvinhliem1307@gmail.com>
2022-07-26 16:06:48 +0700

    Done for wifi QR

--- app.py
+++ app.py
@@ -9,17 +9,27 @@ app.config['SECRET_KEY'] = "supersecretkey"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect(url_for('normal'), code=302)
+    return redirect(url_for('qr'), code=302)
 
 
-@app.route('/normal', methods=['GET', 'POST'])
-def normal():
+@app.route('/qr', methods=['GET', 'POST'])
+def qr():
     if request.method == 'POST':
-        input_val = request.form.get('inputText')
-        qrcode = normal_generator(input_val)
-        return render_template('index.html', qrcode=qrcode, input_val=input_val)
+        if request.args.get('type') == 'normal':
+            input_val = request.form.get('inputText')
+            qrcode = normal_generator(input_val)
+            return render_template('index.html', qrcode=qrcode)
+        elif request.args.get('type') == 'wifi':
+            ssid = request.form.get('ssid')
+            pwd = request.form.get('password')
+            qrcode = wifi_generator(ssid, pwd)
+            return render_template('wifi.html', qrcode=qrcode)
+    if 'type' in request.args:
+        if request.args.get('type') == 'normal':
+            return render_template('index.html')
+        elif request.args.get('type') == 'wifi':
+            return render_template('wifi.html')
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
